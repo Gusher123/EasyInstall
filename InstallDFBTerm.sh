2658550b3450d02b3f91061fe4282297cb679388
Gusher123 <31935813+Gusher123@users.noreply.github.com>
2017-09-14 21:48:56 +0200

    Update InstallDFBTerm.sh

--- InstallDFBTerm.sh
+++ InstallDFBTerm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+server2="https://github.com/Gusher123/EasyInstall/raw/master"
+
 case $1 in
     "nostart")
 	# Make new /usr/local directory
@@ -7,16 +9,16 @@ case $1 in
 	# Copy the original /usr/local/ files
 	cp -R /usr/local/* /data/hack/local_mnt/
 	# Add the DFBTerm files
-	curl https://dl.dropboxusercontent.com/u/22813771/DFBTerm.tgz -o /tmp/DFBTerm.tgz
+	curl $server2/DFBTerm.tgz -o /tmp/DFBTerm.tgz
 	tar -x -f /tmp/DFBTerm.tgz -C /data/hack/
 	# Install an extra init.d file to bind the /data/hack/local_mnt/ to /usr/local automatically at bootup 
-	curl https://dl.dropboxusercontent.com/u/22813771/24DFBTerm.sh -o /data/hack/init.d/24DFBTerm.sh
+	curl $server2/24DFBTerm.sh -o /data/hack/init.d/24DFBTerm.sh
 	chmod 777 /data/hack/init.d/24DFBTerm.sh
 	# Install a script to kill the Boxee software and start the terminal on HDMI out
-	curl https://dl.dropboxusercontent.com/u/22813771/DFBTerm.sh -o /data/hack/bin/DFBTerm.sh
+	curl $server2/DFBTerm.sh -o /data/hack/bin/DFBTerm.sh
 	chmod 777 /data/hack/bin/DFBTerm.sh
 	# Install a shell with no password 
-	curl https://dl.dropboxusercontent.com/u/22813771/shell_no_password.sh -o /data/hack/misc/shell_no_password.sh
+	curl $server2/shell_no_password.sh -o /data/hack/misc/shell_no_password.sh
 	chmod 777 /data/hack/misc/shell_no_password.sh
 	# Bind the /data/hack/local_mnt/ to /usr/local 
 	sh /data/hack/init.d/24DFBTerm.sh
@@ -27,16 +29,16 @@ case $1 in
 	# Copy the original /usr/local/ files
 	cp -R /usr/local/* /data/hack/local_mnt/
 	# Add the DFBTerm files
-	curl https://dl.dropboxusercontent.com/u/22813771/DFBTerm.tgz -o /tmp/DFBTerm.tgz
+	curl $server2/DFBTerm.tgz -o /tmp/DFBTerm.tgz
 	tar -x -f /tmp/DFBTerm.tgz -C /data/hack/
 	# Install an extra init.d file to bind the /data/hack/local_mnt/ to /usr/local automatically at bootup 
-	curl https://dl.dropboxusercontent.com/u/22813771/24DFBTerm.sh -o /data/hack/init.d/24DFBTerm.sh
+	curl $server2/24DFBTerm.sh -o /data/hack/init.d/24DFBTerm.sh
 	chmod 777 /data/hack/init.d/24DFBTerm.sh
 	# Install a script to kill the Boxee software and start the terminal on HDMI out
-	curl https://dl.dropboxusercontent.com/u/22813771/DFBTerm.sh -o /data/hack/bin/DFBTerm.sh
+	curl $server2/DFBTerm.sh -o /data/hack/bin/DFBTerm.sh
 	chmod 777 /data/hack/bin/DFBTerm.sh
 	# Install a shell with no password 
-	curl https://dl.dropboxusercontent.com/u/22813771/shell_no_password.sh -o /data/hack/misc/shell_no_password.sh
+	curl $server2/shell_no_password.sh -o /data/hack/misc/shell_no_password.sh
 	chmod 777 /data/hack/misc/shell_no_password.sh
 	# Bind the /data/hack/local_mnt/ to /usr/local 
 	sh /data/hack/init.d/24DFBTerm.sh
